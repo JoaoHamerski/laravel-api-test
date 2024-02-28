c6d0931f0b0a444a60b59ac3d06b56342b4189b2
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-02-28 20:04:24 -0300

    feat(sale): add api endpoint to create a new sale

--- domains/Sales/Routes/api.php
+++ domains/Sales/Routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use Domains\Sales\Controllers\CreateSaleController;
 use Domains\Sales\Controllers\GetSalesController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('sales.')->prefix('vendas')->group(function () {
     Route::get('/', GetSalesController::class)->name('get');
+    Route::post('/', CreateSaleController::class)->name('create');
 });
