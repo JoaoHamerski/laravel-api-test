74f9005e2ac29457e8dd96a37593044e5bd2e641
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-02-28 15:33:53 -0300

    chore: remove pre-built laravel frontend stuff

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,14 +1,9 @@
 import { PageProps as InertiaPageProps } from '@inertiajs/core';
-import { AxiosInstance } from 'axios';
 import { route as ziggyRoute } from 'ziggy-js';
 import { PageProps as AppPageProps } from './';
 
 declare global {
-    interface Window {
-        axios: AxiosInstance;
-    }
-
-    var route: typeof ziggyRoute;
+    const route: typeof ziggyRoute;
 }
 
 declare module 'vue' {
