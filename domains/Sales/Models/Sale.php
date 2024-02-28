06c07ac275e418738435c03ea675b31c712508a2
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-02-28 16:45:44 -0300

    feat: add HasFactory to Sale model

--- domains/Sales/Models/Sale.php
+++ domains/Sales/Models/Sale.php
@@ -2,9 +2,11 @@
 
 namespace Domains\Sales\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
+    use HasFactory;
     //
 }
