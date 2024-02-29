b84347edcd93b93208cf468054f4574d83368311
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-02-29 13:34:41 -0300

    feat(sale): add option to merge same sale products

---
+++ domains/Products/Actions/MergeSaleProductsAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Domains\Products\Actions;
+
+use Domains\Products\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
+
+class MergeSaleProductsAction
+{
+    public static function execute(Collection $products)
+    {
+        return $products->groupBy('id')->map(function ($groupedProducts) {
+            $product = new Product([...$groupedProducts[0]->toArray()]);
+
+            $product->id = $groupedProducts[0]->id;
+            $product->amount = $groupedProducts->count();
+            $product->price = $groupedProducts->sum('price');
+
+            return $product;
+        });
+    }
+}
--- domains/Sales/Resources/SaleResource.php
+++ domains/Sales/Resources/SaleResource.php
@@ -2,9 +2,13 @@
 
 namespace Domains\Sales\Resources;
 
+use Domains\Products\Actions\MergeSaleProductsAction;
+use Domains\Products\Models\Product;
 use Domains\Products\Resources\ProductResource;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\MissingValue;
 
 class SaleResource extends JsonResource
 {
@@ -17,8 +21,21 @@ class SaleResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'amount' => 100,
-            'products' => ProductResource::collection($this->whenLoaded('products'))
+            'amount' => round($this->amount, 2),
+            'products' => $this->getProducts()
         ];
+    }
+
+    public function getProducts()
+    {
+        $products = $this->whenLoaded('products');
+
+        if ($products instanceof MissingValue) {
+            return new MissingValue();
+        }
+
+        return ProductResource::collection(
+            MergeSaleProductsAction::execute($products)
+        );
     }
 }
